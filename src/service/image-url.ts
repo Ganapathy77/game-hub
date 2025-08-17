b3dc4922cc8494869b13bb5cc4341c66615a4684
Ganapathy77 <ganapathy14777@gmail.com>
2025-08-17 21:13:36 +0530

    No image games handled.

--- src/service/image-url.ts
+++ src/service/image-url.ts
@@ -1,5 +1,7 @@
+import noImgae from "../assets/no-image-placeholder.webp";
+
 const getCroppedImageUrl = (imageUrl: string) => {
-  if (!imageUrl) return "";
+  if (!imageUrl) return noImgae;
   const target = "media/";
   const index = imageUrl.indexOf(target) + target.length;
   return imageUrl.slice(0, index) + "crop/600/400/" + imageUrl.slice(index);
