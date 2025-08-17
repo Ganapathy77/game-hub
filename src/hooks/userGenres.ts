32518029b428ce100d12218d6edb2001022389c4
Ganapathy77 <ganapathy14777@gmail.com>
2025-08-17 18:34:17 +0530

    Create genre component.

---
+++ src/hooks/userGenres.ts
@@ -0,0 +1,41 @@
+import { number } from "framer-motion";
+import { useEffect, useState } from "react";
+import apiClient from "../service/api-client";
+
+interface Genre {
+  id: number;
+  name: string;
+  slug: string;
+}
+
+interface FetchGenreResponse {
+  count: number;
+  results: Genre[];
+}
+
+const useGenres = () => {
+  const [genres, setGenres] = useState<Genre[]>([]);
+  const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const controller = new AbortController();
+    setIsLoading(true);
+    apiClient
+      .get<FetchGenreResponse>("/genres", { signal: controller.signal })
+      .then((res) => {
+        setGenres(res.data.results);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        setError(err.message);
+        setIsLoading(false);
+      });
+
+    return () => controller.abort();
+  }, []);
+
+  return { genres, error, isLoading };
+};
+
+export default useGenres;
